a1f3f2bbd9d8c91c8e2b416271b781efbed140aa
Stefan Binder <binder_stefan@outlook.com>
2023-11-11 08:58:29 +0100

    Add README to package metadata

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import json
-import os
+from pathlib import Path
+
 from setuptools import setup
 
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
-with open('package.json') as f:
+with open("package.json") as f:
     package = json.load(f)
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
@@ -11,13 +14,15 @@ package_name = package["name"].replace(" ", "_").replace("-", "_")
 setup(
     name=package_name,
     version=package["version"],
-    author=package['author'],
+    author=package["author"],
     packages=[package_name],
     include_package_data=True,
-    license=package['license'],
-    description=package.get('description', package_name),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license=package["license"],
+    description=package.get("description", package_name),
     install_requires=[],
-    classifiers = [
-        'Framework :: Dash',
-    ],    
+    classifiers=[
+        "Framework :: Dash",
+    ],
 )
